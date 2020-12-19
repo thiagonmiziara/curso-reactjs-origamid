6e2c525aceca5ca80da8d10c65fd452e06e15f18
thiagonmiziara <thiagonmiziara@gmail.com>
2020-12-19 16:23:31 -0300

    finalizando aula sobre props

--- ex-componentes/componentes/src/App.js
+++ ex-componentes/componentes/src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import {Header} from '../src/components/Header';
-import Footer from '../src/components/Footer';
-import './App.css';
-import Form from './components/Form';
+import React from "react";
+import { Header } from "../src/components/Header";
+import Footer from "../src/components/Footer";
+import "./App.css";
+import Form from "./components/Form";
 
-const App=()=> {
+const Titulo = ({cor,texto, children}) => {
+  return <h1 style={{color:cor}}>{texto},{children}</h1>;
+};
+
+const App = () => {
   return (
     <div className="App">
-      <Header/>
-      <Form/>
-      <Footer/>
+      <Titulo cor="red" texto="Meu titulo 1" >Isso é o CHILDREN</Titulo>
+      <Titulo cor="blue" texto="Meu titulo 2" />
+
+      <Header />
+      <Form />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
