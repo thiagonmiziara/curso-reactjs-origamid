3844ac517b0940e8e9c86268bf5736d73dcea8ca
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-11 17:05:31 -0300

    Add component radio

--- componentInput/component-input/src/App.js
+++ componentInput/component-input/src/App.js
@@ -1,22 +1,40 @@
 import React from "react";
 import "./App.css";
 import Input from "./Form/Input";
+import Radio from "./Form/Radio";
 import Select from "./Form/Select";
 
 const App = () => {
   const [nome, setNome] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [produto, setProduto] = React.useState("");
+  const [cor, setCor] = React.useState("Vermelho");
+  const [fruta, setFruta] = React.useState("Uva");
+
 
   return (
     <div className="App">
       <form>
+        <h2>Cores</h2>
+        <Radio 
+          options={['Azul', 'Vermelho']}
+          value={cor}
+          setValue={setCor}
+        />
+
+        <h2>Frutas</h2>
+
+        <Radio 
+          options={['Limão', 'Laranja', 'Uva']}
+          value={fruta}
+          setValue={setFruta}
+        />
+
         <Select
           options={["Smartphone", "Tablet"]}
           value={produto}
           setValue={setProduto}
         />
-
         <Input
           id="nome"
           label="Nome"
@@ -24,9 +42,9 @@ const App = () => {
           setValue={setNome}
           required
         />
-        <Input id="email" label="Email" value={email} setValue={setEmail} />
-        <button>Enviar</button>
-      </form>
+        <Input id="email" label="Email" value={email} setValue={setEmail} />{" "}
+        <button> Enviar </button>{" "}
+      </form>{" "}
     </div>
   );
 };
