fefd3798291d81970ae3b9c0e338efa7916c030a
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-11 16:21:45 -0300

    Add componente select

--- componentInput/component-input/src/App.js
+++ componentInput/component-input/src/App.js
@@ -1,15 +1,29 @@
 import React from "react";
 import "./App.css";
 import Input from "./Form/Input";
+import Select from "./Form/Select";
 
 const App = () => {
   const [nome, setNome] = React.useState("");
   const [email, setEmail] = React.useState("");
+  const [produto, setProduto] = React.useState("");
 
   return (
     <div className="App">
       <form>
-        <Input id="nome" label="Nome" value={nome} setValue={setNome} required />
+        <Select
+          options={["Smartphone", "Tablet"]}
+          value={produto}
+          setValue={setProduto}
+        />
+
+        <Input
+          id="nome"
+          label="Nome"
+          value={nome}
+          setValue={setNome}
+          required
+        />
         <Input id="email" label="Email" value={email} setValue={setEmail} />
         <button>Enviar</button>
       </form>
