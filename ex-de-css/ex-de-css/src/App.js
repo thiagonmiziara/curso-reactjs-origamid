95c203289478c14c18523667c27804e9f8615df7
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-12 19:32:08 -0300

    Add css module exemplos

---
+++ ex-de-css/ex-de-css/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Produto from './components/Produto';
+
+const App = () => {
+  return (
+    <div className="App">
+     <Produto/>
+    </div>
+  );
+}
+
+export default App;
