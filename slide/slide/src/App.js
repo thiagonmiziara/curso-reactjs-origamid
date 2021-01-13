5ec23efbffc715536b65d517717b30703fc1e575
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-13 17:22:13 -0300

    Add svg e img e componente svg

--- slide/slide/src/App.js
+++ slide/slide/src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
+import "./App.css";
 import Slide from "./Slide";
+import Img from "./img/foto.jpg";
+import { ReactComponent as Dog } from "./img/dog.svg";
+import DogSvg from "./DogSvg";
 
 const App = () => {
-  
+  const [olho ,setOlho] = React.useState(0);
+
+  function handleClik(){
+    for(let i =0; i <6; i++){
+      setTimeout(()=>{
+        setOlho(i);
+      },100 * i)
+    }
+    
+  }
+
   return (
     <div>
+      <Dog />
+      <p className="fundo" onClick={handleClik}></p>
+      <img className="fundo" src={Img} alt="Cachorro" />
       <Slide />
+      <DogSvg color="#84e" olho={olho} />
     </div>
   );
 };
--- slide/slide/src/Slide.jsx
+++ slide/slide/src/Slide.jsx
@@ -1,57 +1,68 @@
 import React from "react";
 import styles from "./Slide.module.css";
-import foto1 from './thiago.png'
-import foto2 from './foto2.jpg'
-import foto3 from './foto3.jpg'
-import foto4 from './foto4.jpg'
-
+import foto1 from "./thiago.png";
+import foto2 from "./foto2.jpg";
+import foto3 from "./foto3.jpg";
+import foto4 from "./foto4.jpg";
+import DogSvg from "./DogSvg";
 
 const slides = [
   {
-    id: 'slide1',
-    image: foto1
+    id: "slide1",
+    image: foto1,
   },
   {
-    id: 'slide2',
-    image: foto2
+    id: "slide2",
+    image: foto2,
   },
   {
-    id: 'slide3',
-    image: foto3
+    id: "slide3",
+    image: foto3,
   },
   {
-    id: 'slide4',
-    image: foto4
+    id: "slide4",
+    image: foto4,
   },
-]
+];
+
 const Slide = () => {
   const [active, setActive] = React.useState(0);
   const [position, setPosition] = React.useState(0);
   const contentRef = React.useRef();
 
-  React.useEffect(()=>{
+  const [olho ,setOlho] = React.useState(0);
 
-    const{width} = contentRef.current.getBoundingClientRect();
+  function handleClik(){
+    for(let i =0; i <6; i++){
+      setTimeout(()=>{
+        setOlho(i);
+      },100 * i)
+    }
+    
+  }
+
+  React.useEffect(() => {
+    const { width } = contentRef.current.getBoundingClientRect();
     setPosition(-(width * active));
-
-
-  },[active])
-
+  }, [active]);
 
   function slidePrev() {
-    if(active > 0){
-    setActive(active - 1)
+    if (active > 0) {
+      setActive(active - 1);
     }
+    handleClik()
   }
 
   function slideNext() {
-    if(active < slides.length - 1){
-    setActive(active + 1)
+    if (active < slides.length - 1) {
+      setActive(active + 1);
     }
+    handleClik()
   }
 
   return (
     <section className={styles.container}>
+       <DogSvg color="#84e" olho={olho} />
       <div
         ref={contentRef}
         className={styles.content}
@@ -59,7 +70,7 @@ const Slide = () => {
       >
         {slides.map((slide) => (
           <div key={slide.id} className={styles.item}>
-            <img className={styles.img}src={slide.image} alt="Thiago"/>
+            <img className={styles.img} src={slide.image} alt="Thiago" />
           </div>
         ))}
       </div>
