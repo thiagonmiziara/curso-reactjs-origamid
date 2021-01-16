12bb59b2ac4355d80db52a92d108ead74119058d
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-16 19:27:47 -0300

    Projeto finalizando

--- desafio-rotas/desafio-rotas/src/Components/Produto/index.jsx
+++ desafio-rotas/desafio-rotas/src/Components/Produto/index.jsx
@@ -25,7 +25,7 @@ const Produto = () => {
     fetchData(`https://ranekapi.origamid.dev/json/api/produto/${id}`);
   }, [id]);
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <div className="loading"></div>;
   if (error) return <p>{error}</p>;
   if (produto === null) return null;
   return (
@@ -34,9 +34,11 @@ const Produto = () => {
         title={produto.nome}
         description={` Esse é um produto ${produto.nome}`}
       />
-      {produto.fotos.map((foto) => (
-        <img key={foto.src} src={foto.src} alt={foto.titulo} />
-      ))}
+      <div>
+        {produto.fotos.map((foto) => (
+          <img key={foto.src} src={foto.src} alt={foto.titulo} />
+        ))}
+      </div>
       <div>
         <h1>{produto.nome}</h1>
         <span className={styles.preco}>R$ {produto.preco}</span>
