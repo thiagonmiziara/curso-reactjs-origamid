b6d72432459913fab84a33fb83507eee2e4eedd5
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-18 19:54:34 -0300

    apredendo sobre o React.memo

--- propTypes/proptypes/src/App.js
+++ propTypes/proptypes/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Button from "./Button";
+import Header from "./Memo/Header";
 const Contato = React.lazy(() => import("./Lazy/Contato"));
 
 const App = () => {
   const [ativo, setAtivo] = React.useState(false);
+  const [contar, setContar] = React.useState(0);
 
   return (
     <div>
+      <div>
+        <Header/>
+        <button onClick={()=> setContar(contar +1)}>{contar}</button>
+      </div>
       <h1>propTypes</h1>
       <Button width={200}>Clique Aqui</Button>
       <div>
