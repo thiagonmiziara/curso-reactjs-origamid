6d35b44b4662f772954073eaaaba3a95633316c7
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-18 19:01:22 -0300

    Add lazy e suspense

--- propTypes/proptypes/src/App.js
+++ propTypes/proptypes/src/App.js
@@ -1,17 +1,25 @@
-import Button from "./Button"
+import React from "react";
+import Button from "./Button";
+const Contato = React.lazy(() => import("./Lazy/Contato"));
 
+const App = () => {
+  const [ativo, setAtivo] = React.useState(false);
 
-
-const  App = () => {
   return (
-    <div >
-   <h1>propTypes</h1>
-
-   <Button width={200} >
-      Clique Aqui
-   </Button>
+    <div>
+      <h1>propTypes</h1>
+      <Button width={200}>Clique Aqui</Button>
+      <div>
+        <h1>Contato</h1>
+        {ativo && (
+          <React.Suspense fallback={<div>Carregando...</div>}>
+            <Contato />
+          </React.Suspense>
+        )}
+        <button onClick={()=> setAtivo(true)}>Clique</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
